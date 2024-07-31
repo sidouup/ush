fd2e243a61666cd818590abd37b19e387f1fc80e
sidouup <160077558+sidouup@users.noreply.github.com>
2024-07-31 15:51:19 -0400

    Update stats.py

--- pages/stats.py
+++ pages/stats.py
@@ -18,18 +18,92 @@ def get_google_sheet_client():
 
 # Function to load data from Google Sheets
 def load_data(spreadsheet_id):
-    client = get_google_sheet_client()
-    sheet = client.open_by_key(spreadsheet_id)
-    worksheets = sheet.worksheets()
-    combined_data = pd.DataFrame()
+    sheet_headers = {
+        'PAYMENT & MAIL': [
+            'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'APPLICATION': [
+            'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'SCAN & SEND': [
+            'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'ARAMEX & RDV': [
+            'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'DS-160': [
+            'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'ITW Prep.': [
+            'DATE', 'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'SEVIS': [
+            'DATE', 'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'CLIENTS ': [
+            'DATE', 'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ]
+    }
+    
+    try:
+        client = get_google_sheet_client()
 
-    for worksheet in worksheets:
-        data = worksheet.get_all_records()
-        df = pd.DataFrame(data)
-        df['Worksheet'] = worksheet.title
-        combined_data = pd.concat([combined_data, df], ignore_index=True)
+        sheet = client.open_by_key(spreadsheet_id)
+  
+        
+        combined_data = pd.DataFrame()
+        
+        for worksheet in sheet.worksheets():
+            title = worksheet.title
+            expected_headers = sheet_headers.get(title, None)
+            
+            if expected_headers:
+                data = worksheet.get_all_records(expected_headers=expected_headers)
+            else:
+                data = worksheet.get_all_records()
+            
+            df = pd.DataFrame(data)
+            if not df.empty:
+                if 'First Name' in df.columns and 'Last Name' in df.columns:
+                    df['First Name'] = df['First Name'].astype(str)
+                    df['Last Name'] = df['Last Name'].astype(str)
+                    df['Student Name'] = df['First Name'] + " " + df['Last Name']
+                df.dropna(subset=['Student Name'], inplace=True)
+                df.dropna(how='all', inplace=True)
+                df['Current Step'] = title
+                combined_data = pd.concat([combined_data, df], ignore_index=True)
+        
+        combined_data.drop_duplicates(subset='Student Name', keep='last', inplace=True)
+        combined_data.reset_index(drop=True, inplace=True)
 
-    return combined_data
+        return combined_data
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        return pd.DataFrame()
 
 # Main function for the statistics page
 def statistics_page():
