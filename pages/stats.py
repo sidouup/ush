5a0f2e298ca3bbdd8ff6e53c46a914d79acb457e
sidouup <160077558+sidouup@users.noreply.github.com>
2024-07-31 16:29:10 -0400

    Update stats.py

--- pages/stats.py
+++ pages/stats.py
@@ -1,3 +1,110 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from google.oauth2.service_account import Credentials
+import gspread
+
+# Use Streamlit secrets for service account info
+SERVICE_ACCOUNT_INFO = st.secrets["gcp_service_account"]
+
+# Define the scopes
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+# Authenticate and build the Google Sheets service
+@st.cache_resource
+def get_google_sheet_client():
+    creds = Credentials.from_service_account_info(SERVICE_ACCOUNT_INFO, scopes=SCOPES)
+    return gspread.authorize(creds)
+
+
+def load_data(spreadsheet_id):
+    sheet_headers = {
+        'PAYMENT & MAIL': [
+            'DATE','First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'APPLICATION': [
+            'DATE','First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'SCAN & SEND': [
+            'DATE','First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'ARAMEX & RDV': [
+            'DATE','First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'DS-160': [
+            'DATE','First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'ITW Prep.': [
+            'DATE', 'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'SEVIS': [
+            'DATE', 'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ],
+        'CLIENTS ': [
+            'DATE', 'First Name', 'Last Name', 'Phone N°', 'Address', 'E-mail', 'Emergency contact N°', 'Chosen School',
+            'Duration', 'Payment Method ', 'Sevis payment ? ', 'Application payment ?', 'DS-160 maker', 'Password DS-160',
+            'Secret Q.', 'School Entry Date', 'Entry Date in the US', 'ADDRESS in the U.S', ' E-MAIL RDV', 'PASSWORD RDV',
+            'EMBASSY ITW. DATE', 'Attempts', 'Visa Result', 'Agent', 'Note'
+        ]
+    }
+    
+    try:
+        client = get_google_sheet_client()
+
+        sheet = client.open_by_key(spreadsheet_id)
+  
+        
+        combined_data = pd.DataFrame()
+        
+        for worksheet in sheet.worksheets():
+            title = worksheet.title
+            expected_headers = sheet_headers.get(title, None)
+            
+            if expected_headers:
+                data = worksheet.get_all_records(expected_headers=expected_headers)
+            else:
+                data = worksheet.get_all_records()
+            
+            df = pd.DataFrame(data)
+            if not df.empty:
+                if 'First Name' in df.columns and 'Last Name' in df.columns:
+                    df['First Name'] = df['First Name'].astype(str)
+                    df['Last Name'] = df['Last Name'].astype(str)
+                    df['Student Name'] = df['First Name'] + " " + df['Last Name']
+                df.dropna(subset=['Student Name'], inplace=True)
+                df.dropna(how='all', inplace=True)
+                df['Current Step'] = title
+                combined_data = pd.concat([combined_data, df], ignore_index=True)
+        
+        combined_data.drop_duplicates(subset='Student Name', keep='last', inplace=True)
+        combined_data.reset_index(drop=True, inplace=True)
+
+        return combined_data
+    except Exception as e:
+        st.error(f"An error occurred: {str(e)}")
+        return pd.DataFrame()
+        
 def plot_insights(data):
     # Ensure the DATE column is datetime type
     data['DATE'] = pd.to_datetime(data['DATE'], errors='coerce')
